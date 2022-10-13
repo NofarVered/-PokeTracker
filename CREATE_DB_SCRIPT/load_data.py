f928ab2834f87bc1710fa5988677288d3ea4cba9
Itay.Talias <itay.talias@gmail.com>
2022-10-13 17:39:49 +0300

    load from JSOM file + first insert query

--- CREATE_DB_SCRIPT/load_data.py
+++ CREATE_DB_SCRIPT/load_data.py
@@ -1,18 +1,26 @@
+import json
 
-# class load_data:
 
-# @staticmethod
-# def load_pokemon_from_json_file(path):
-#     # לפרסר קובץ גסון ולהפעיל 4 פונקציות
+class load_data:
 
-# @staticmethod
-# def load_pokemons_info_table(): itay
+    @staticmethod
+    def load_pokemon_from_json_file(path):
+        f = open(path)
+        pokemon_data = json.load(f)
+        f.close()
+        for pokemon in pokemon_data:
+            load_data.load_pokemons_info_table(
+                pokemon["name"], pokemon["weight"])
+            load_data.load_pokemon_type_table("???", pokemon["type"])
 
-# @staticmethod
-# def load_trainers_info_table(): nofar
+    # @staticmethod
+    # def load_pokemons_info_table(pokemon_name, pokemon_weight):
 
-# @staticmethod
-# def load_pokemon_type_table(): itay
+    #     # @staticmethod
+    #     # def load_trainers_info_table(): nofar
 
-# @staticmethod
-# def load_pokemon_pokemons_table(): nofar
+    # @staticmethod
+    # def load_pokemon_type_table(pokemon_id, pokemon_type):
+
+    #     # @staticmethod
+    #     # def load_pokemon_pokemons_table(): nofar
