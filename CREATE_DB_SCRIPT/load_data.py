faea8e7e1c058bb79b184ee53cf87ab775593bbd
Itay.Talias <itay.talias@gmail.com>
2022-10-13 16:45:00 +0300

    config

---
+++ CREATE_DB_SCRIPT/load_data.py
@@ -0,0 +1,18 @@
+
+class load_data:
+
+    @staticmethod
+    def load_pokemon_from_json_file(path):
+        # לפרסר קובץ גסון ולהפעיל 4 פונקציות
+
+    @staticmethod
+    def load_pokemons_info_table():
+
+    @staticmethod
+    def load_trainers_info_table():
+
+    @staticmethod
+    def load_pokemon_type_table():
+
+    @staticmethod
+    def load_pokemon_pokemons_table():
