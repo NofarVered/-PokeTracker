866df0b782f9a509915f5da0422a1267dc1baf8e
NofarVered <nofarss79@gmail.com>
2022-10-14 02:12:23 +0300

    select query 1

--- API_DATA_RETRIEVE/querys.py
+++ API_DATA_RETRIEVE/querys.py
@@ -1,11 +1,14 @@
 
-# SELECT_HEAVIEST_POKEMON= nofar
+SELECT_HEAVIEST_POKEMON = """SELECT MAX(weight) AS LargestWeight, name FROM pokemons_info"""
+
 SELECT_POKEMONS_BY_TYPE = """SELECT pokemons_info.name 
                             FROM pokemons_info 
                             JOIN pokemon_types 
                             ON pokemons_info.pokemons_id = pokemon_types.pokemons_id
                             WHERE type LIKE ($s)"""
-# SELECT_TRAINERS_BY_POKEMON= nofar
+
+SELECT_TRAINERS_BY_POKEMON = """SELECT """
+
 SELECT_POKEMONS_BY_TRAINERS = """SELECT pokemons_info.name 
                             FROM pokemons_info 
                             JOIN trainer_pokemons 
@@ -13,4 +16,5 @@ SELECT_POKEMONS_BY_TRAINERS = """SELECT pokemons_info.name
                             JOIN trainers_info
                             ON trainers_info.trainer_id = trainer_pokemons.trainer_id
                             WHERE trainers_info.name LIKE ($s)"""
+
 # SELECT_POPULAR_POKEMON= nofar + itay
