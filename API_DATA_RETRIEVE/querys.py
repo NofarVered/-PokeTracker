9a62feb3d3b47df273eb7d770d3e97a5a5ef9630
Itay.Talias <itay.talias@gmail.com>
2022-10-14 19:14:07 +0300

    update query

--- API_DATA_RETRIEVE/querys.py
+++ API_DATA_RETRIEVE/querys.py
@@ -6,19 +6,15 @@ SELECT_POKEMONS_BY_TYPE = """SELECT pokemons_info.name
                             FROM pokemons_info 
                             JOIN pokemon_types 
                             ON pokemons_info.pokemons_id = pokemon_types.pokemons_id
-                            WHERE type LIKE ($s)"""
+                            WHERE type LIKE %s"""
 
 SELECT_TRAINERS_BY_POKEMON = """SELECT pokemon_name, trainer_name 
                                 FROM Trainers_by_pokemon 
-                                WHERE pokemon_name = %s """
+                                WHERE pokemon_name LIKE %s """
 
 SELECT_POKEMONS_BY_TRAINERS = """SELECT pokemons_info.name 
-                            FROM pokemons_info 
-                            JOIN trainer_pokemons 
-                            ON pokemons_info.pokemons_id = trainer_pokemons.pokemons_id
-                            JOIN trainers_info
-                            ON trainers_info.trainer_id = trainer_pokemons.trainer_id
-                            WHERE trainers_info.name LIKE ($s)"""
+                            FROM Trainers_pokemons_view
+                            WHERE trainer_name LIKE %s """
 
 SELECT_POPULAR_POKEMON = """SELECT pokemon_name, COUNT(trainer_name) AS number_of_trainers
                             FROM Trainers_by_pokemon 
