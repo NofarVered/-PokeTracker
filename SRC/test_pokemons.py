eb2ebcae4f169166141fe37b93d0c113125db3aa
Itay.Talias <itay.talias@gmail.com>
2022-10-20 12:29:27 +0300

    get types of pokemon - test

--- SRC/test_pokemons.py
+++ SRC/test_pokemons.py
@@ -1,4 +1,3 @@
-from unicodedata import name
 from fastapi.testclient import TestClient
 from server import app
 from fastapi import status
@@ -12,3 +11,11 @@ def test_get_pokemon_by_type():
     assert response.status_code == status.HTTP_200_OK, " status code in get pokemon by type isn't 200"
     assert "eevee" in [pokemon["name"]
                        for pokemon in response_body], "eevee isn't in the list"
+
+
+def test_get_types_of_pokemon():
+    response = client.get("/pokemons/eevee")
+    response_body = response.json()["types"]
+    assert response.status_code == status.HTTP_200_OK, " status code in get types of pokemon isn't 200"
+    assert len(response_body) == len(set(response_body)
+                                     ), "one of the elements appears twice in the list"
