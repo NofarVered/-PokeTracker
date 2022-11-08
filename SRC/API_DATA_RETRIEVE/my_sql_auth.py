a7307c78fd84d39aeee939ea794b2fe8e88ffeb2
Avishay Mezeg <Avishay.Mezeg@cyberark.com>
2022-11-08 16:19:15 +0200

    code review

--- SRC/API_DATA_RETRIEVE/my_sql_auth.py
+++ SRC/API_DATA_RETRIEVE/my_sql_auth.py
@@ -8,7 +8,7 @@ DEFAULT_DB = "sql_poketracker"
 DEFAULT_PWD = ""
 
 
-class my_sql_auth:
+class my_sql_auth: # consider to tansform to dataclass // camel case
     def __init__(self, user: str = DEFAULT_USER, pwd: str = DEFAULT_PWD, db: str = DEFAULT_DB, host: str = DEFAULT_HOST):
         self.host = host,
         self.user = user,
--- SRC/server.py
+++ SRC/server.py
@@ -1,5 +1,8 @@
-from fastapi import FastAPI
+import pymysql
+from fastapi import FastAPI, Request
 import uvicorn
+from starlette.responses import JSONResponse
+
 from CREATE_DB_SCRIPT.load_data import load_data
 from API_DATA_RETRIEVE.my_sql_proxy import CONNECTOR
 from ROUTERS import trainers_router, pokemons_router, evolve_router
@@ -15,6 +18,14 @@ app.include_router(evolve_router.router)
 @app.get("/")
 def root():
     return "server is up and running"
+
+
+@app.exception_handler(pymysql.MySQLError)
+async def mysql_connector_error_handling(request: Request, exc: pymysql.MySQLError):
+    return JSONResponse(
+        status_code=500,
+        content={"message": f"Oops! {exc.name} did something. There goes a rainbow..."},
+    )
 
 
 if __name__ == "__main__":
